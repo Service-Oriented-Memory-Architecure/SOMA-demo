9ed9efdb69b72b7fc5da2805cb3627d4bef73506
Joseph Melber <jgmelber@gmail.com>
2020-07-17 13:50:33 -0400

    Update csr.h

--- SOMA-examples/03_bfs/bfs_gen_ccip/sw/csr.h
+++ SOMA-examples/03_bfs/bfs_gen_ccip/sw/csr.h
@@ -20,8 +20,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 //
-// Service module that generates messages to services from software MMIO CSR writes.
-//
 // BFS specialized
 #ifndef CSR_H
 #define CSR_H
